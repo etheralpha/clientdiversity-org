bdaa916bf38bd8c61b8aabcfa8b05bf7e92e8b87
hanniabu <hanniabu@gmail.com>
2023-04-05 18:00:30 -0400

    add toast notification

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,4 @@
 {% include js/base.js %}
 {% include js/clientdistribution.js %}
 {% include js/switchclients.js %}
-{%- if site.notification_enabled == true -%}
-  {% include js/notification.js %}
-{%- endif -%}
 
